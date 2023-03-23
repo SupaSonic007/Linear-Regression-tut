c2288e8c75a2f6d3d92d9e1c707eb4e1d0f279db
joush007 <joshua.kisnorbo@stu.burgmann.act.edu.au>
2023-03-24 10:30:14 +1100

    Start implementation
    Errors still present including 2 runtime warnings meaning unable to produce output just yet

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,47 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 df = pd.read_csv('ex1data1.txt', header = None)
 print(df.head())
+
+theta = [0, 0]
+
+def hypothesis(theta, X):
+    return theta[0]+theta[1]*X
+
+def cost_calc(theta, X, y):
+    #! RuntimeWarning: overflow encountered in double_scalars
+    return (1/2*m) * np.sum((hypothesis(theta, X)-y)**2)
+
+
+m = len(df)
+def gradient_descent(theta, X, y, epoch, alpha):
+    
+    cost = []
+    i = 0
+    
+    while i < epoch:
+        hx = hypothesis(theta, X)
+        #! RuntimeWarning: invalid value encountered in double_scalars
+        theta[0] -= alpha * (sum(hx-y)/m)
+        theta[1] -= (alpha * np.sum((hx-y)*X))/m
+        cost.append(cost_calc(theta, X, y))
+        i+=1
+    
+    return theta, cost
+
+def predict(theta, X, y, epoch, alpha):
+    theta, cost = gradient_descent(theta, X, y, epoch, alpha)
+    return hypothesis(theta, X), cost, theta
+
+y_predict, cost, theta = predict(theta, df[0],df[1], 1000, 0.1)
+
+plt.figure()
+plt.scatter(df[0], df[1], label="Original Y")
+plt.scatter(df[0], y_predict, label="Predicted Y")
+plt.legend(loc="upper left")
+plt.xlabel("input feature")
+plt.ylabel("original and predicted output")
+
+plt.show()
